2ee6b2baadbe379e4ac78196089b584747f29320
Sara <vanvugt.sara@gmail.com>
2020-04-15 19:51:35 -0400

    test

--- server.py
+++ server.py
@@ -54,8 +54,17 @@ class Battlesnake(object):
         return "ok"
     
 class snake:
+    
     def __init__(self, request):
         self.request = request
+        
+    def get_next_move(self):
+        head = self.get_head_coords()
+        if head["x"] == 0:
+             return "up"
+        return "left"
+     
+        
      
          
 
