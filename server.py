7e9982dc442b5082bc3e97410e2db0d8c5a1d90b
Sara <vanvugt.sara@gmail.com>
2020-04-16 16:08:13 -0400

    test

--- server.py
+++ server.py
@@ -38,9 +38,7 @@ class Battlesnake(object):
         # Valid moves are "up", "down", "left", or "right".
         # TODO: Use the information in cherrypy.request.json to decide your next move.
         data = cherrypy.request.json
-        snake = snake(data)
-        move = snake.get_next_move
-        
+   
         
         print(f"MOVE: {move}")
         return {"move": move}
@@ -56,12 +54,7 @@ class Battlesnake(object):
         return "ok"
         
         
-@dataclass
-class Snake:
-        def __init__(self, request):
-            self.request = request
-    
-    
+
      
 if __name__ == "__main__":
     server = Battlesnake()
